73375e039ad1641302947cc12a346ed81f3b2441
viralsolani <viral.solani@gmail.com>
2012-05-26 12:50:07 +0530

    change config.php

--- config.php
+++ config.php
@@ -5,6 +5,6 @@
  * A single location to store configuration.
  */
 
-define('CONSUMER_KEY', 'BNBttT0y9ZG3gfztMUyMJg');
-define('CONSUMER_SECRET', 'b1EtOX2l0o8abDW9auyMtttR1rLhqa5TNLn47yVSk');
-define('OAUTH_CALLBACK', 'http://www.devmeworld.com/rightway/twitteroauth/callback.php');
+define('CONSUMER_KEY', 'Your CONSUMER_KEY');
+define('CONSUMER_SECRET', 'Your CONSUMER_SECRET');
+define('OAUTH_CALLBACK', 'Your OAUTH_CALLBACK');
